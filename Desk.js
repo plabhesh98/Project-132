82e013d5d92d3114535115f21bccde1f46249e16
plabhesh98 <81747071+plabhesh98@users.noreply.github.com>
2022-05-06 15:38:18 +0530

    Add files via upload

---
+++ Desk.js
@@ -0,0 +1,32 @@
+img="";
+Status = "";
+
+function preload(){
+    img = loadImage("desk.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.position(450, 250);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Cocossd is Initialized");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
